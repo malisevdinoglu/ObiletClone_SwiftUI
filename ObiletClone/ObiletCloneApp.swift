379c5d19f666b11ec6cd165a4a7686d36769b6c1
Coders Club <hi@codersclub.co>
2025-09-15 11:33:46 +0300

    Initial Commit

---
+++ ObiletClone/ObiletCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObiletCloneApp.swift
+//  ObiletClone
+//
+//  Created by Mehmet Ali Sevdinoğlu on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ObiletCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
